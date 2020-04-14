377c9237a6ed8efaad5ced7eda740c2e7ce2ad35
dpat5941 <dpat59411@gmail.com>
2020-04-14 21:10:27 +1000

    User can select points and draw wall

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # Path-Finding
- Path finding visualization 
+ Path finding visualization with <b> A* algorithm</b>
+ <br><br>
+ <b> Modules - </b><br>
+ <a href="https://www.pygame.org/">pygame</a> and <a href="https://wiki.python.org/moin/TkInter">tkinter</a>
+ <br><br>
+ <b>Language - </b>
+ <br>Python
+ 
+<br><br>
+<strong> How Program works? </strong><br>
+Run the program(main.py) and after that a box will open up. Choose two points. After that user can draw wall with Mouse and then press Space key to start Algorithm. Path can't pass through wall.
+ <br><br>
+ 
+ The <b>A* algorithm </b> works on principle of<br><center> F = G + H</center></br> 
+ <br>
+ <ul>
+ <li> F is the total cost of node.</li>
+ <li> G is distance between the current node and start node</li>
+ <li> H is the heuristic - estimated distance from current node to end node.</li>
+ </ul>
+<img src="https://miro.medium.com/max/600/1*iSt-urlSaXDABqhXX6xveQ.png" alt="A* algorithm">
+<br>
+Let's assume node(0) is starting position and node(19) is end position. The current node is on node(4).
+<br><br>
+<strong>H</strong>
+<br>
+H is estimated distance from current node to end node. node(19) isover 7 spaces and 3 spaces from node(4). 
+<br>
+Here, use Pythagorus theorem,<br>
+ a² + b² = c²<br>
+ So, for currentnode the value of H is 7² + 3² = 58.
+ <br>
+ <strong> Note - </strong>
+<br><p> Even if we don't apply the square root to 58, we will still get same output so skip that calculation. </p>
+<br>
+It's important that the estimated distance is always underestimation of total path or overstimation will lead to A* searching for nodes which are not <i>best</i> in terms of <b>F</b>.
+<br><br>
+<b>G</b><br>
+G is the distance between current node and start node.
+<br>
+node(4) is 4 spaces away from initial node. So, value of G for currentNode is 4.
+<br><br>
+<b>F</b>
+<br>
+F is total cost of the node. So, value of F for currentNode is sum of G and H of currentNode which is 4+58 = 62.
+<br><br>
+<b> Why F?</b><br>
+<img src="https://miro.medium.com/max/263/1*HppvOLfDxXqQRFn0Cv2dHQ.gif">
+Rather than checking all node, pick the ones that have the highest chance of getting us to our goal with help of F value.
+<br><br>
+<b> Why not Djikstra?</b>
+<br>
+<img src="https://miro.medium.com/max/263/1*2jRCHqAbTCY7W7oG5ntMOQ.gif">
+Take a good look. The Dijkstra keeps searching. It has no idea which node is 'good' and how we can achieve best result so it calculates paths for all available nodes.
+<br>
+<br>
+
+<strong> Pseudo Code</strong>
+<br>
+Check out pseudo code for A* from <a href="http://www.policyalmanac.org/games/aStarTutorial.htm">Patrick Lester's blog</a>. 
+<br>
+<br>1. Add the starting square (or node) to the open list.
+<br>2. Repeat the following:<br>
+<br>A) Look for the lowest F cost square on the open list. We refer to this as the current square.
+<br>B). Switch it to the closed list.
+<br>C) For each of the 8 squares adjacent to this current square …<br>
+<br><i>!)If it is not walkable or if it is on the closed list, ignore it. Otherwise do the following.
+<br>!!)If it isn’t on the open list, add it to the open list. Make the current square the parent of this square. Record the F, G, and H costs of the square.
+<br>!!!)If it is on the open list already, check to see if this path to that square is better, using G cost as the measure. A lower G cost means that this is a better path. If so, change the parent of the square to the current square, and recalculate the G and F scores of the square. If you are keeping your open list sorted by F score, you may need to resort the list to account for the change.
+<br><b>D)</b></i> Stop when you:
+Add the target square to the closed list, in which case the path has been found, or
+Fail to find the target square, and the open list is empty. In this case, there is no path.
+<br><br>3. Save the path. Working backwards from the target square, go from each square to its parent square until you reach the starting square. That is your path.
---
+++ main.py
@@ -0,0 +1,197 @@
+import pygame
+import sys
+import math
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import os
+
+
+col = 50
+w = 750 / col
+row = 50
+h = 750 / row
+grids = [0 for x in range(col)]
+openSet = []
+closedSet = []
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+grey = (220, 220, 220)
+cameFrom = []
+
+
+window = Tk()
+label = Label(window, text="Start(x-point, y-point): ")
+labelEnd = Label(window, text="End(x-point, y-point): ")
+startBox = Entry(window)
+endBox = Entry(window)
+var = IntVar()
+showPath = ttk.Checkbutton(window, text="The Steps ", onvalue=1, offvalue=0, variable=var)
+
+
+# Visual Screen
+
+vScreen = pygame.display.set_mode((750, 750))
+
+
+class Square:
+    # The node square
+
+    def __init__(self,xval, yval):
+
+        self.x = xval       # X axis
+        self.y = yval       # Y axis
+        self.f = 0          # F value
+        self.g = 0          # G value
+        self.h = 0          # H value
+        # F = G + H
+        self.neighbor = []
+        self.last = None
+        self.obs = False
+        self.close = False
+        self.val = 1
+
+    def show(self, colour, st):
+        # Draw the square
+
+        if self.close is False:
+            pygame.draw.rect(vScreen, colour, (self.x * w, self.y * h, w, h),st)
+            pygame.display.update()
+
+    def path(self, colour, st):
+        # The path finding square
+
+        pygame.draw.rect(vScreen, colour, (self.x * w, self.y * h, w, h),st)
+        pygame.display.update()
+
+    def addNeighbor(self, grids):
+        x = self.x
+        y = self.y
+        if x < col-1 and grids[self.x + 1][y].obs is False:
+            self.neighbor.append(grids[self.x + 1][y])
+        if x > 0 and grids[self.x - 1][y].obs is False:
+            self.neighbor.append(grids[self.x - 1][y])
+        if y < row-1 and grids[self.x][y + 1].obs is False:
+            self.neighbor.append(grids[self.x][y + 1])
+        if y > 0 and grids[self.x][y - 1].obs is False:
+            self.neighbor.append(grids[self.x][y - 1])
+
+
+# Draw 2d Array
+for x in range(col):
+    grids[x] = [0 for x in range(row)]
+
+
+# Create squares
+for x in range(col):
+    for y in range(row):
+        grids[x][y] = Square(x, y)
+
+# Default initial and target node
+
+start = grids[3][5]
+end = grids[12][5]
+
+# Show squares
+for x in range(col):
+    for y in range(row):
+        grids[x][y].show((255, 255, 153), 1)
+
+for x in range(0, row):
+    grids[0][x].show(blue, 0)
+    grids[0][x].obs = True
+    grids[col - 1][x].obs = True
+    grids[col - 1][x].show(blue, 0)
+    grids[x][row - 1].show(blue, 0)
+    grids[x][0].show(blue, 0)
+    grids[x][0].obs = True
+    grids[x][row-1].obs = True
+
+
+def submit():
+    global start
+    global end
+    st = startBox.get().split(",")
+    ed = endBox.get().split(",")
+    start = grids[int(st[0])][int(st[1])]
+    end = grids[int(ed[0])][int(ed[1])]
+    window.quit()
+    window.destroy()
+
+
+Start = Button(window, text="Start", command=submit)
+
+showPath.grid(columnspan=2, row=2)
+Start.grid(columnspan=2, row=3)
+labelEnd.grid(row=1, pady=3)
+endBox.grid(row=1, column=1, pady=3)
+startBox.grid(row=0, column=1, pady=3)
+label.grid(row=0, pady=3)
+
+
+window.update()
+mainloop()
+
+pygame.init()
+openSet.append(start)
+
+
+def mousePressed(val):
+
+    # Wall between two points is defined with mousePress
+
+    t = val[0]
+    w = val[1]
+    g1 = t // (750 // col)
+    g2 = w // (750 // row)
+    access = grids[g1][g2]
+    if access != start and access != end:
+        if access.obs is False:
+            access.obs = True
+            access.show((255, 255, 255), 0)
+
+
+end.show((255,8,127), 0)
+start.show((255,8,127), 0)
+
+looping = True
+
+while looping:
+
+    # Draw wall and after pressing space run the program
+    # Wall is white
+    event = pygame.event.get()
+
+    for ev in event:
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+
+        if pygame.mouse.get_pressed()[0]:
+            try:
+                position = pygame.mouse.get_pos()
+                mousePressed(position)
+            except AttributeError:
+                pass
+
+        elif ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_SPACE:
+                looping = False
+                break
+
+# Add all neighbours
+
+for x in range(col):
+    for y in range(row):
+        grids[x][y].addNeighbor(grids)
+
+# H value for F = G + H
+
+
+def h_value(num, ex):
+    dist = math.sqrt((num.x - ex.x)**2 + (num.y - ex.j)**2)
+    return dist
+
+
+
+
